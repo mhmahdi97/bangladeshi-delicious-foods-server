4afbe535548acb439de75ca4d7137ab917c8bbac
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-04 14:11:03 +0600

    created api to get chefs and recipes data

--- index.js
+++ index.js
@@ -8,11 +8,28 @@ const port = process.env.port || 5000;
 app.use(cors());
 
 // fetching data from json file
+const chefs = require('./data/chefData.json');
+const recipes = require('./data/recipeData.json');
 
+// creating api
 app.get('/', (req, res)=> {
     res.send('chef recipe hunter server')
 })
 
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
+})
+
+
+
+
+
+// listening port
 app.listen(port, ()=> {
     console.log('recipe hunter is runnig')
 })
